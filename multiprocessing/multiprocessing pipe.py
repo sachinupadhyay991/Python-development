9bf29d1fe25ae77d146649d36bce140cf52f5e76
sachinupadhyay991 <33253002+sachinupadhyay991@users.noreply.github.com>
2020-05-28 18:12:53 +0530

    Update multiprocessing pipe.py

--- multiprocessing/multiprocessing pipe.py
+++ multiprocessing/multiprocessing pipe.py
@@ -1,19 +1,12 @@
 import multiprocessing 
 
 def sender(conn, msgs): 
-	""" 
-	function to send messages to other end of pipe 
-	"""
 	for msg in msgs: 
 		conn.send(msg) 
 		print("Sent the message: {}".format(msg)) 
 	conn.close() 
 
 def receiver(conn): 
-	""" 
-	function to print the messages received from other 
-	end of pipe 
-	"""
 	while 1: 
 		msg = conn.recv() 
 		if msg == "END": 
